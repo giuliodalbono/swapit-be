59070a80a3d31b9c6baf990285deae5317c53ee2
giuliodalbono <dalbonogiulio@gmail.com>
2025-10-10 18:51:35 +0200

    modified rate skill event

--- src/main/kotlin/io/github/giuliodalbono/swapit/service/producer/FeedbackEventProducer.kt
+++ src/main/kotlin/io/github/giuliodalbono/swapit/service/producer/FeedbackEventProducer.kt
@@ -3,10 +3,12 @@ package io.github.giuliodalbono.swapit.service.producer
 import io.github.giuliodalbono.swapit.dto.FeedbackDto
 import io.github.giuliodalbono.swapit.service.CloudEventContext
 import io.github.giuliodalbono.swapit.service.CloudEventService
+import io.github.giuliodalbono.swapit.service.UserService
 import org.springframework.stereotype.Service
 
 @Service
 class FeedbackEventProducer(
+    private val userService: UserService,
     private val cloudEventService: CloudEventService
 ) {
 
@@ -17,7 +19,20 @@ class FeedbackEventProducer(
     }
 
     fun produceRateSkillEvent(feedback: FeedbackDto) {
-        val cloudEventContext = CloudEventContext(subject = SKILL_SUBJECT, type = RATE_TYPE, data = feedback)
+        val skillRateEvent = feedback.toSkillRateEvent(userService)
+        val cloudEventContext = CloudEventContext(subject = SKILL_SUBJECT, type = RATE_TYPE, data = skillRateEvent)
         cloudEventService.sendCloudEventAfterCommit(cloudEventContext, FEEDBACK_TOPIC)
     }
 }
+
+data class SkillRateEvent(val rating: Long, val review: String, val reviewerUid: String, val reviewedUid: String, val skills: Set<String>)
+
+fun FeedbackDto.toSkillRateEvent(userService: UserService) = SkillRateEvent(
+    rating = this@toSkillRateEvent.rating,
+    review = this@toSkillRateEvent.review,
+    reviewerUid = this@toSkillRateEvent.reviewerUid!!,
+    reviewedUid = this@toSkillRateEvent.reviewedUid!!,
+    skills = userService.findByUid(this@toSkillRateEvent.reviewedUid)
+        .orElseThrow()
+        .skillOffered!!
+)
